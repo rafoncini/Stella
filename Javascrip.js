aa2b6ee69c853660e933c39ba8d5e6605751349d
rafoncini <142694741+rafoncini@users.noreply.github.com>
2025-03-25 18:21:10 -0400

    Add files via upload

---
+++ Javascrip.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-personalizar').addEventListener('click', function() {
+    var menu = document.getElementById('personalizacion-menu');
+    menu.classList.toggle('show'); // Alterna entre mostrar u ocultar el menú
+});
